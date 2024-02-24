d291b27c05f3fd511ae39fbcfab3b304419577c8
Besco <bernardbolter@gmail.com>
2024-02-24 17:23:34 +0100

    updated impressum

--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -26,8 +26,8 @@ const Impressum = () => {
                 <h2>AUTOR DIESES INTERNETANGEBOTES</h2>
                 <h2 className="spacer">UND INHABER VON ARTISM</h2>
                 <h5>Bernard John Bolter IV</h5>
-                <h5>Cuvry Strasse 32</h5>
-                <h5>10997 Berlin</h5>
+                <h5>Markgrafendamm 18</h5>
+                <h5>10245 Berlin</h5>
                 <h4>email: bernardbolter@gmail.com</h4>
                 <h5>DE: +49 (0)151 4749 1553</h5>
                 <h5>US: +1 415 490 7702 <span>what&#39;s app, telegram, and signal</span></h5>
